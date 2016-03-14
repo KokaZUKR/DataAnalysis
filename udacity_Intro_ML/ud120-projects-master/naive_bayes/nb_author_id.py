180259eeac973d7768e8045ef668a680ce784d3c
Artem Z <azraev@croc.ru>
2016-03-15 00:48:51 +0300

    Update kinopoisk file. Total DF is created.

---
+++ udacity_Intro_ML/ud120-projects-master/naive_bayes/nb_author_id.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+
+""" 
+    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project. 
+
+    Use a Naive Bayes Classifier to identify emails by their authors
+    
+    authors and labels:
+    Sara has label 0
+    Chris has label 1
+"""
+    
+import sys
+from time import time
+sys.path.append("../tools/")
+from email_preprocess import preprocess
+
+
+### features_train and features_test are the features for the training
+### and testing datasets, respectively
+### labels_train and labels_test are the corresponding item labels
+features_train, features_test, labels_train, labels_test = preprocess()
+
+def NBAccuracy(features_train, labels_train, features_test, labels_test):
+    """ compute the accuracy of your Naive Bayes classifier """
+    ### import the sklearn module for GaussianNB
+    from sklearn.naive_bayes import GaussianNB
+    import sklearn.metrics
+
+    ### create classifier
+    clf = GaussianNB()
+
+    ### fit the classifier on the training features and labels
+    clf.fit(features_train, labels_train)
+
+    ### use the trained classifier to predict labels for the test features
+    pred = clf.predict(features_test)
+
+
+    ### calculate and return the accuracy on the test data
+    ### this is slightly different than the example, 
+    ### where we just print the accuracy
+    ### you might need to import an sklearn module
+    accuracy = sklearn.metrics.accuracy_score(labels_test, pred)
+    return accuracy
+
+NBAccuracy(features_train, features_test, labels_train, labels_test)
+#########################################################
+### your code goes here ###
+
+
+#########################################################
+
+
